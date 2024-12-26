25e81aac4d5edb2b0a32cb44912ee2744e802ed7
nickTit <nptitok@hotmail.com>
2024-12-27 02:10:32 +0300

    added_inventory

--- oop_game.py
+++ oop_game.py
@@ -1,4 +1,5 @@
 import keyboard
+import random
 
 arena = [
     ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],#-13
@@ -15,19 +16,25 @@ arena = [
     ["|", " ", " ", " ", "|", " ", "|", " ", " ", " ", " ", "|"],#-2
     ["|", "_", "_", "_", "|", "H", "|", "_", "_", "_", "_", "|"] #-1
 ]
-def print_arena():
-    for i in arena:
-        print(" ".join(i), "\n")
 
+def print_arena():
+    keys = [key for key in inventary]
+    
+    counter = 0
+    for i in arena:
+        
+        if counter != len(inventary):
+            print(f"{" ".join(i)} {counter}: {inventary[keys[counter]]}")
+            counter+=1
+        else:
+            print(" ".join(i))
 
 """for i in arena1:
     for j in i:
         arena_unpacked.append(j)"""
 arena_unpacked = [j for i in arena for j in i]
 
-#print(arena_unpacked)
 class hero():
-
     def __init__(self,x=5,y=-1):
         self.__x = x
         self.__y = y
@@ -42,19 +49,24 @@ class hero():
    
     def move_to(self,direction):
         x,y = self.cords
-        if direction == "w" and arena[y-1][x]== " ":
+        possible_moves = [" ", "s", "с", "л", "з"]
+        if direction == "w" and arena[y-1][x] in possible_moves:
+            self.pick_up_item(y-1,x)
             self.__y -=1
-        elif direction == "s" and arena[y+1][x]== " ":
+        elif direction == "s" and arena[y+1][x] in possible_moves:
+            self.pick_up_item(y+1,x)
             self.__y +=1
-        elif direction =="a" and arena[y][x-1]== " ":
+        elif direction =="a" and arena[y][x-1] in possible_moves:
+            self.pick_up_item(y,x-1)
             self.__x -=1       
-        elif direction =="d" and arena[y][x+1]== " ":
+        elif direction =="d" and arena[y][x+1] in possible_moves:
+            self.pick_up_item(y,x+1)
             self.__x+=1
         print(self.cords)
         arena[y][x] = "z"
         arena[self.__y][self.__x] = "H"
         print_arena()
-    
+        
 
     """def move_left(self):
         x,y = self.cords
@@ -103,22 +115,77 @@ class hero():
             keyboard.wait()
         except IndexError:
             print("НЕВОЗМОЖНО ДВИГАТЬСЯ")#по сути не работает т.к. arena[y][x+1]== " " не даёт уйти за пределы 
-            
 
-
-             
+    def pick_up_item(self,new_y, new_x):
+        inventory_norm = {"з":"essence","л":"catcher","с":"wall","s":"sword"}
+        unpacked = [i for i in inventory_norm]
+        #print(unpacked)
+        item_name="---"
+        if arena[new_y][new_x] in unpacked:
+            item_name=inventory_norm[arena[new_y][new_x]] 
+            print(item_name)
+            inv.set_item(item_name)
+        print(inv.inventary)
+        #print(inv.essence, "sdasdad")
         
 
 
 
-knight = hero()
+class inventory(): #меч(1) зелье здоровья(3)(з) ловушка(3)(л) стена(5)(с)
+    
+    def __init__(self):
+        self.items = {
+            "sword": False,
+            "essence": 0,
+            "wall": 0,
+            "catcher": 0
+        }
 
+        self.inventary = {"Меч":f"{self.items["sword"]}", "Зелье здоровья": f"{self.items["essence"]}", "Стена": f"{self.items["wall"]}", "Ловушка": f"{self.items["catcher"]}"}
+    
+    def set_item(self,item_name):
+        self.items[item_name]+=1
+        self.inventary = {"Меч":f"{self.items["sword"]}", "Зелье здоровья": f"{self.items["essence"]}", "Стена": f"{self.items["wall"]}", "Ловушка": f"{self.items["catcher"]}"}
+
+        #каждый раз обновляется, чтобы был актуальный список
+
+    def create_item(self, item ,count):
+        for i in range(count):
+            while True:
+                obj_x = random.randint(0,11)
+                obj_y = random.randint(0,len(arena)-1)
+                if arena[obj_y][obj_x] == " ": 
+                    arena[obj_y][obj_x] = f"{item}"
+                    break
+        
+
+    def show_arena_items(self):
+        arena[-2][5] = 's'
+        self.create_item("л",3)
+        self.create_item("з",3)
+        self.create_item("с", 5)
+
+        
+    def use_item(self):
+        pass
+
+
+
+knight = hero()
+inv = inventory()
+
+#test
+inventary = inv.inventary
+knight.pick_up_item(1,2)
+#test
+
+inv.show_arena_items()
+knight.move_hero()
 
 print(knight.cords, "\n")
 
 #knight.cords = (2,2)
 
-knight.move_hero()
 
 
 
